77c391af93071198c9167dc42e897964f268b9ec
Vic Kuo <eun.hru@gmail.com>
2020-03-16 11:42:15 +0800

    update2

--- app.py
+++ app.py
@@ -37,11 +37,9 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-	msg = event.message.text
-    s = '你吃飯了嗎'
-    line_bot_api.reply_message(
+	line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=s))
+        TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
